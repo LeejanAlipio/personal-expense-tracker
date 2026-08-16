e4ab3e356b4cf11851499f516d7983c1c66e7a9c
Lee <alipioml0131@gmail.com>
2026-08-16 19:24:05 +0800

    refactor: reorder initialization functions for better application startup flow

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import './styles/style.css';
-import { initValidation } from './modules/ui.js';
+import { initValidation, renderExpenses } from './modules/ui.js';
+import { loadExpenses } from './modules/storage.js';
 
+loadExpenses();
 initValidation();
+renderExpenses();
