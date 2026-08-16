8d1358afeb5b969eb4ced551ac1689004867ad3e
Lee <alipioml0131@gmail.com>
2026-08-16 16:40:11 +0800

    feat: add utils function for calculating total and average

---
+++ src/utils/calculate.js
@@ -0,0 +1,10 @@
+import { expenseTracker } from '../modules/expense';
+
+export function getTotal() {
+  return expenseTracker.getExpenses().reduce((sum, expense) => sum + expense.amount, 0);
+}
+
+export function getAverage() {
+  const expenses = expenseTracker.getExpenses().length;
+  return expenses.length === 0 ? 0 : getTotal() / expenses.length;
+}
